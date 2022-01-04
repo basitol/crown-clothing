f13f4fa43d236a36ec7d8826aa6ec1a256b980ef
basitol <41623944+basitol@users.noreply.github.com>
2022-01-04 15:16:59 +0100

    implemented firebase utility, included ability to store authenticated user in database

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+// import { useRouteMatch } from "react-router-dom";
 
 const config = {
   apiKey: "AIzaSyCL0s-nt1JGbFULzL1ygXPaHEjY3w9cxTM",
@@ -10,6 +11,34 @@ const config = {
   messagingSenderId: "425240601126",
   appId: "1:425240601126:web:e8cca52319e3f8fa933ce7",
   measurementId: "G-QWZTVWEPSH",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createDat = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createDat,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
